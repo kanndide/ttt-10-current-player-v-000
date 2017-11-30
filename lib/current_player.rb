febfcc4747b0107d3966a4d9de422ca17167aef9
William Kann <william.l.kann@gmail.com>
2017-11-30 17:02:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turn_count = 0
   board.each do | turns |
-    if #{board} == "X" || "O"
+    if #{turns} == "X" || "O"
       turn_count =+ 1
     else
       turn_count = 0
